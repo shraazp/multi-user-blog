b839e9b1de67ca098d5e666705f7aebee59877b2
shraazp <shravyap99@gmail.com>
2022-05-13 10:23:45 +0530

    added:navigation bar and footer to the home page

---
+++ src/firebase/firebaseInit.js
@@ -0,0 +1,17 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+const firebaseConfig = {
+    apiKey: "AIzaSyDBtOQRmjNucP8IrhveeTJOMD1mbu4NzEk",
+    authDomain: "vueblogs-73053.firebaseapp.com",
+    projectId: "vueblogs-73053",
+    storageBucket: "vueblogs-73053.appspot.com",
+    messagingSenderId: "165420075202",
+    appId: "1:165420075202:web:7aa6b247b96020774cd35b",
+    measurementId: "G-3ZKNB2251S"
+  };
+
+  const firebaseApp=firebase.initializeApp(firebaseConfig);
+  const timestamp=firebase.firestore.FieldValue.serverTimestamp;
+
+  export {timestamp };
+  export default firebaseApp.firestore;
---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  components: {},
+  data(){
+    return {
+      welcomeScreen:{
+        title:"welcome",
+        
+      }
+    }
+  }
+};
+</script>
